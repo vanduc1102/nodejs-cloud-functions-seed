9795ba8560b477957b4eb16206f87395f43a3686
Duc Nguyen <vanducld@gmail.com>
2019-05-27 15:12:57 +0700

    update port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const express = require('express');
 
 const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = 4000;
 
 const routers = require('./src/routers/index');
 
